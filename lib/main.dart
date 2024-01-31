7f523f9a43f33a1bf9819e8ac3204fec8d73a769
Chris Friemann <github@chris.friemann.nl>
2024-01-31 14:34:37 +0100

    cleanup: removed not needed comments and code

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   List<CameraDescription> cameras = await availableCameras();
 
-  if (cameras.isNotEmpty) {
-    CameraDescription firstCamera = cameras.first;
-    // Now you can use the 'firstCamera' object as needed
-    print('There are ${cameras.length} cameras available');
-  } else {
-    print('No cameras available');
-  }
+  CameraDescription firstCamera = cameras.first;
 
   runApp(MyApp(
     cameras: cameras,
@@ -61,44 +55,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     List<CameraDescription> camera = widget.cameras;
-    print('Build cam ${camera}');
-    print('Build cam 2 ${camera.first}');
-    // CameraDescription camera = widget.cameras.first;
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: const Color.fromRGBO(30, 36, 56, 1),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title, style: TextStyle(color: Colors.grey[300])),
       ),
       body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Text(
           'Zoek of scan een boek',
           style: TextStyle(
@@ -115,18 +81,13 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             IconButton(
               icon: const Icon(
-                  Icons.home_outlined), // replace with your desired icon
+                  Icons.home_outlined),
               color: Colors.grey[300],
               style: ButtonStyle(
-                // this backgroundColor makes it usable on any background color, here set to transparent
                 iconSize: MaterialStateProperty.all(40.0),
               ),
               onPressed: () {
-                // Show the dialog
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const NewPage()),
-                );
+                // 
               },
             ),
             Expanded(
@@ -135,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 decoration: BoxDecoration(
                   color: Colors.grey[300],
                   borderRadius: BorderRadius.circular(
-                      10.0), // this makes the corners rounded
+                      10.0),
                 ),
                 child: const Center(
                   child: TextField(
@@ -154,20 +115,18 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             IconButton(
               icon:
-                  const Icon(Icons.qr_code_2), // replace with your desired icon
+                  const Icon(Icons.qr_code_2),
               color: Colors.grey[300],
               style: ButtonStyle(
-                // this backgroundColor makes it usable on any background color, here set to transparent
                 iconSize: MaterialStateProperty.all(40.0),
               ),
               onPressed: () {
-                // Show the dialog
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => TakePictureScreen(
-                            camera: camera.first,
-                          )),
+                  builder: (context) => TakePictureScreen(
+                    camera: camera.first,
+                  )),
                 );
               },
             ),
@@ -177,26 +136,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-// // Create the camera screen
-// class NewPage extends StatelessWidget {
-//   const NewPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Camera'),
-//       ),
-//       body: const Center(
-//         child: Text(
-//           'Camera',
-//           style: TextStyle(
-//             fontSize: 30.0,
-//             color: Colors.black,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
